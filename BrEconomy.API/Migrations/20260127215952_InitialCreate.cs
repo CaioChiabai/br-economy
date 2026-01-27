4f8282b61cb42b6d63c2cca44a0daecd9ed815f4
Caio Chiabai <caiochiabaioliveira@gmail.com>
2026-01-27 19:17:31 -0300

    feat: initial migration

---
+++ BrEconomy.API/Migrations/20260127215952_InitialCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BrEconomy.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EconomicIndicators",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<decimal>(type: "numeric", nullable: false),
+                    ReferenceDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EconomicIndicators", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EconomicIndicators_Name",
+                table: "EconomicIndicators",
+                column: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EconomicIndicators");
+        }
+    }
+}
