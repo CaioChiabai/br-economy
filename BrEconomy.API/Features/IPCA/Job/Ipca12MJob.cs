c708e3512fa5cd5901f9c97b295e2711a31014ab
Caio Chiabai <caiochiabaioliveira@gmail.com>
2026-01-31 11:39:26 -0300

    feat: implementado buscas da taxa IPCA

---
+++ BrEconomy.API/Features/IPCA/Job/Ipca12MJob.cs
@@ -0,0 +1,22 @@
+﻿using BrEconomy.API.Features.Selic.Job;
+using BrEconomy.API.Features.Shared;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace BrEconomy.API.Features.IPCA.Job
+{
+    public class Ipca12MJob : GenericIndicatorUpdateJob
+    {
+        protected override string Name => "IPCA_12M";
+        protected override string Url => "dados/serie/bcdata.sgs.13522/dados/ultimos/1?formato=json";
+        protected override string CacheKey => "indicador:ipca:12m";
+
+        public Ipca12MJob(
+            IServiceProvider serviceProvider,
+            IHttpClientFactory httpClientFactory,
+            IDistributedCache cache,
+            ILogger<SelicCurrentJob> logger)
+            : base(serviceProvider, httpClientFactory, cache, logger)
+        {
+        }
+    }
+}
---
+++ BrEconomy.API/Features/IPCA/Job/IpcaYtdJob.cs
@@ -0,0 +1,22 @@
+﻿using BrEconomy.API.Features.Selic.Job;
+using BrEconomy.API.Features.Shared;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace BrEconomy.API.Features.IPCA.Job
+{
+    public class IpcaYtdJob : GenericIndicatorUpdateJob
+    {
+        protected override string Name => "IPCA_YTD";
+        protected override string Url => "dados/serie/bcdata.sgs.433/dados/ultimos/1?formato=json";
+        protected override string CacheKey => "indicador:ipca:ytd";
+
+        public IpcaYtdJob(
+            IServiceProvider serviceProvider,
+            IHttpClientFactory httpClientFactory,
+            IDistributedCache cache,
+            ILogger<SelicCurrentJob> logger)
+            : base(serviceProvider, httpClientFactory, cache, logger)
+        {
+        }
+    }
+}
