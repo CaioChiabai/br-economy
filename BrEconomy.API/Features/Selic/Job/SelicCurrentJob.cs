e936af390dd0c1bbfc3e54596c10d3ab5a64cca1
Caio Chiabai <caiochiabaioliveira@gmail.com>
2026-01-31 11:35:32 -0300

    refactor: modificado nome da classe para deixar mais claro e padronizado

--- BrEconomy.API/Features/Selic/Job/SelicCurrentJob.cs
+++ BrEconomy.API/Features/Selic/Job/SelicCurrentJob.cs
@@ -1,19 +1,19 @@
 ﻿using BrEconomy.API.Features.Shared;
 using Microsoft.Extensions.Caching.Distributed;
 
-namespace BrEconomy.API.Features.Selic;
+namespace BrEconomy.API.Features.Selic.Job;
 
-public class SelicUpdateJob : GenericIndicatorUpdateJob
+public class SelicCurrentJob : GenericIndicatorUpdateJob
 {
     protected override string Name => "SELIC";
     protected override string Url => "dados/serie/bcdata.sgs.432/dados/ultimos/1?formato=json";
-    protected override string CacheKey => "indicador:selic";
+    protected override string CacheKey => "indicador:selic:current";
 
-    public SelicUpdateJob(
+    public SelicCurrentJob(
         IServiceProvider serviceProvider,
         IHttpClientFactory httpClientFactory,
         IDistributedCache cache,
-        ILogger<SelicUpdateJob> logger)
+        ILogger<SelicCurrentJob> logger)
         : base(serviceProvider, httpClientFactory, cache, logger)
     {
     }
