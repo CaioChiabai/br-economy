3462b158cc1e2f99a9c7dbfe9a355766292a10e2
Caio Chiabai <caiochiabaioliveira@gmail.com>
2026-02-09 10:33:02 -0300

    x

--- BrEconomy.API/Program.cs
+++ BrEconomy.API/Program.cs
@@ -10,12 +10,27 @@ var builder = WebApplication.CreateBuilder(args);
 // --- 1. Configuração do Banco (Postgres) ---
 var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
+if (string.IsNullOrEmpty(databaseUrl))
+{
+    throw new Exception("DATABASE_URL não configurada.");
+}
+
+var uri = new Uri(databaseUrl);
+var userInfo = uri.UserInfo.Split(':');
+
+// Porta padrão 5432 se não vier explícita
+var port = uri.Port > 0 ? uri.Port : 5432;
+
+var connectionString =
+    $"Host={uri.Host};" +
+    $"Port={port};" +
+    $"Database={uri.AbsolutePath.TrimStart('/')};" +
+    $"Username={userInfo[0]};" +
+    $"Password={userInfo[1]};" +
+    "SSL Mode=Require;Trust Server Certificate=true;";
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(
-        !string.IsNullOrEmpty(databaseUrl)
-            ? databaseUrl
-            : builder.Configuration.GetConnectionString("DefaultConnection")
-    ));
+    options.UseNpgsql(connectionString));
 
 // --- 2. Configuração do Redis ---
 
