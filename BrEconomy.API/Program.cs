e939c0ca7c39d08c35047a2796080be4348cdf15
Caio Chiabai <caiochiabaioliveira@gmail.com>
2026-02-09 10:03:32 -0300

    fix: atualizado program para suporte a deploy

--- BrEconomy.API/Program.cs
+++ BrEconomy.API/Program.cs
@@ -8,17 +8,44 @@ using BrEconomy.API.Features.CDI.Job;
 var builder = WebApplication.CreateBuilder(args);
 
 // --- 1. Configuração do Banco (Postgres) ---
-// A string de conexão aponta para o Docker que subimos (localhost:5432)
-var connectionString = "Host=localhost;Port=5432;Database=breconomy;Username=postgres;Password=postgres";
+var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+if (!string.IsNullOrEmpty(databaseUrl))
+{
+    var uri = new Uri(databaseUrl);
+    var userInfo = uri.UserInfo.Split(':');
+
+    var connectionString =
+        $"Host={uri.Host};" +
+        $"Port={uri.Port};" +
+        $"Database={uri.AbsolutePath.TrimStart('/')};" +
+        $"Username={userInfo[0]};" +
+        $"Password={userInfo[1]};" +
+        "SSL Mode=Require;Trust Server Certificate=true;";
+
+    builder.Configuration["ConnectionStrings:DefaultConnection"] = connectionString;
+}
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(connectionString));
+    options.UseNpgsql("DefaultConnection"));
 
 // --- 2. Configuração do Redis ---
-builder.Services.AddStackExchangeRedisCache(options =>
+
+var redisUrl = Environment.GetEnvironmentVariable("REDIS_URL")
+               ?? builder.Configuration["Redis:Configuration"];
+
+if (!string.IsNullOrEmpty(redisUrl))
 {
-    options.Configuration = "localhost:6379";
-    options.InstanceName = "BrEconomy_";
-});
+    builder.Services.AddStackExchangeRedisCache(options =>
+    {
+        options.Configuration = redisUrl;
+        options.InstanceName = "BrEconomy_";
+    });
+}
+else
+{
+    builder.Services.AddDistributedMemoryCache();
+}
 
 // --- 3. Configuração do HttpClient ---
 builder.Services.AddHttpClient("BancoCentral", client =>
@@ -48,5 +75,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseAuthorization();
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
